eb5004c1d6ab1b0109a3b2188a78ad017b120a54
Hongyu Huang <31984374+hongyuhuang@users.noreply.github.com>
2022-09-05 21:40:55 +1200

    Update CustomerDAOTest.java

--- src/test/java/dao/CustomerDAOTest.java
+++ src/test/java/dao/CustomerDAOTest.java
@@ -30,9 +30,9 @@ public class CustomerDAOTest {
     @BeforeEach
     public void setUp() {
         customersCollections = new CustomerCollectionsDAO();
-        c1 = new Customer("hagmy545", "Mya", "Hagget", "hagmy45@student.otago.ac.nz", "657 George Street Dunedin", "hagmy545");
-        c2 = new Customer("huaho519", "Hongyu", "Huang", "hongyuhuang963@gmail.com", "35 Clifford Road Wellington", "huaho519");
-        c3 = new Customer("mcgsi765", "Siobhan", "McGhee-Turnock", "mcgsi765@student.otago.ac.nz", "57 Heriot Row Dunedin", "mcgsi765");
+        c1 = new Customer("hagmy", "Mya", "Hagget", "hagmy@student.otago.ac.nz", "57 Heriot Row Dunedin", "hagmy");
+        c2 = new Customer("huaho", "Hongyu", "Huang", "huaho@student.otago.ac.nz", "57 Heriot Row Dunedin", "huaho");
+        c3 = new Customer("mcgsi", "Siobhan", "McGhee-Turnock", "mcgsi@student.otago.ac.nz", "57 Heriot Row Dunedin", "mcgsi");
         customersCollections.save(c1);
         customersCollections.save(c2);
         
@@ -53,59 +53,59 @@ public class CustomerDAOTest {
 
     @Test
     public void testSave() {
-        assertThat(customersCollections.getByUsername("hagmy545"), is(c1));
-        assertThat(customersCollections.getByUsername("huaho519"), is(c2));
-        assertThat(customersCollections.getByUsername("mcgsi765"), not(is(c3)));
+        assertThat(customersCollections.getByUsername("hagmy"), is(c1));
+        assertThat(customersCollections.getByUsername("huaho"), is(c2));
+        assertThat(customersCollections.getByUsername("mcgsi"), not(is(c3)));
 
         customersCollections.save(c3);
 
-        assertThat(customersCollections.getByUsername("mcgsi765"), is(c3));
+        assertThat(customersCollections.getByUsername("mcgsi"), is(c3));
         
-        assertThat(customersJdbi.getByUsername("hagmy545"), is(c1));
-        assertThat(customersJdbi.getByUsername("huaho519"), is(c2));
-        assertThat(customersJdbi.getByUsername("mcgsi765"), not(is(c3)));
+        assertThat(customersJdbi.getByUsername("hagmy"), is(c1));
+        assertThat(customersJdbi.getByUsername("huaho"), is(c2));
+        assertThat(customersJdbi.getByUsername("mcgsi"), not(is(c3)));
 
         customersJdbi.save(c3);
 
-        assertThat(customersJdbi.getByUsername("mcgsi765"), is(c3));
+        assertThat(customersJdbi.getByUsername("mcgsi"), is(c3));
     }
 
     @Test
     public void testMatch() {
-        assertTrue(customersCollections.match("hagmy545", "hagmy545"));
-        assertTrue(customersCollections.match("huaho519", "huaho519"));
-        assertFalse(customersCollections.match("hagmy545", "1234"));
-        assertFalse(customersCollections.match("huaho519", "1234"));
+        assertTrue(customersCollections.match("hagmy", "hagmy"));
+        assertTrue(customersCollections.match("huaho", "huaho"));
+        assertFalse(customersCollections.match("hagmy", "1234"));
+        assertFalse(customersCollections.match("huaho", "1234"));
         
-        assertTrue(customersJdbi.match("hagmy545", "hagmy545"));
-        assertTrue(customersJdbi.match("huaho519", "huaho519"));
-        assertFalse(customersJdbi.match("hagmy545", "1234"));
-        assertFalse(customersJdbi.match("huaho519", "1234"));
+        assertTrue(customersJdbi.match("hagmy", "hagmy"));
+        assertTrue(customersJdbi.match("huaho", "huaho"));
+        assertFalse(customersJdbi.match("hagmy", "1234"));
+        assertFalse(customersJdbi.match("huaho", "1234"));
     }
 
     @Test
     public void testGetByUsername() {
-        assertThat(customersCollections.getByUsername("hagmy545"), is(c1));
-        assertThat(customersCollections.getByUsername("huaho519"), is(c2));
+        assertThat(customersCollections.getByUsername("hagmy"), is(c1));
+        assertThat(customersCollections.getByUsername("huaho"), is(c2));
         
-        assertThat(customersJdbi.getByUsername("hagmy545"), is(c1));
-        assertThat(customersJdbi.getByUsername("huaho519"), is(c2));
+        assertThat(customersJdbi.getByUsername("hagmy"), is(c1));
+        assertThat(customersJdbi.getByUsername("huaho"), is(c2));
     }
 
     @Test
     public void testDelete() {
-        assertThat(customersCollections.getByUsername("hagmy545"), is(c1));
-        assertThat(customersCollections.getByUsername("huaho519"), is(c2));
+        assertThat(customersCollections.getByUsername("hagmy"), is(c1));
+        assertThat(customersCollections.getByUsername("huaho"), is(c2));
 
         customersCollections.delete(c2);
 
-        assertThat(customersCollections.getByUsername("huaho519"), not(is(c2)));
+        assertThat(customersCollections.getByUsername("huaho"), not(is(c2)));
         
-        assertThat(customersJdbi.getByUsername("hagmy545"), is(c1));
-        assertThat(customersJdbi.getByUsername("huaho519"), is(c2));
+        assertThat(customersJdbi.getByUsername("hagmy"), is(c1));
+        assertThat(customersJdbi.getByUsername("huaho"), is(c2));
 
         customersJdbi.delete(c2);
 
-        assertThat(customersJdbi.getByUsername("huaho519"), not(is(c2)));
+        assertThat(customersJdbi.getByUsername("huaho"), not(is(c2)));
     }
 }
